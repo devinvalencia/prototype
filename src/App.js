f4b324d12c6c42e35a6163921f27647168ed9d7a
Devin Valencia <DevinValencia@yahoo.com>
2022-02-03 15:14:41 -0500

    Google styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./Custom Components/Home";
 
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -10,9 +11,10 @@ function App() {
   return (
     <div>
       <Container fluid="sm" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh', width: '100vw'}}>
-        <Row>
-          <Col><Home></Home></Col>
-        </Row>
+          <Col>
+
+          <Home></Home>
+          </Col>
       </Container>
     </div>
   );
