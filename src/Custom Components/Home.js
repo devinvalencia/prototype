dfb1098b0f9cc791c4e17b876a00a90330a3455f
Devin Valencia <DevinValencia@yahoo.com>
2022-02-03 14:39:33 -0500

    Made Simple Form

--- src/Custom Components/Home.js
+++ src/Custom Components/Home.js
@@ -1,14 +1,28 @@
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState, useRef } from 'react';
 
 const Home = () => {
+    const firstName = useRef(null);
+    const [name, setName] = useState('');
 
-    const handleClick = (name, e) => {
-        console.log('Hello! ' + name + e.target);
+    // If no value entered, then send alert
+    const handleClick = () => {
+        setName(`${firstName.current.value} is a poo poo head`);
     }
 
+    // Take form value, assign to name
     return ( 
         <div>
-            <Button onClick={ (e) => handleClick('Mario', e) } variant="primary">Hello!</Button>
+            <Form.Group>
+            <Form.Label>Enter Name</Form.Label>
+            <Form.Control size="lg" type="text" placeholder="..." ref={firstName} />
+            </Form.Group>
+            <br />
+            <Button value='test' onClick={ () => handleClick() } variant="primary">Submit</Button>
+            <br />
+            <br />
+            {name}
         </div>
      );
 }
